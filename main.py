5d7c9dd65985e9b8abd8e8ae48a2a8051814c63b
simmasha <120249387+simmasha@users.noreply.github.com>
2023-06-01 23:45:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
 import random
 
-print("Игра: Камень, ножницы, бумага")
+def generate_random_number():
+    """
+    Generates a random number between 1 and 100
+    """
+    return random.randint(1, 100)
 
-options = ["камень", "ножницы", "бумага"]
-
-while True:
-    player_choice = input("Выберите ваш вариант: ")
-    while player_choice.lower() not in options:
-        print("Некорректный ввод. Попробуйте еще раз.")
-        player_choice = input()
-    
-    computer_choice = random.choice(options)
-    print(f"Вы выбрали {player_choice}, а компьютер выбрал {computer_choice}.")
-    
-    if player_choice.lower() == computer_choice:
-        print("Ничья!")
-    elif (player_choice.lower() == "камень" and computer_choice == "ножницы") or \
-         (player_choice.lower() == "ножницы" and computer_choice == "бумага") or \
-         (player_choice.lower() == "бумага" and computer_choice == "камень"):
-        print("Вы победили!")
-    else:
-        print("Вы проиграли!")
-    
-    play_again = input("Сыграем еще раз? (Да/Нет) ")
-    if play_again.lower() == 'нет':
-        break
-
-print("Спасибо за игру!")
+if __name__ == '__main__':
+    number = generate_random_number()
+    print(f'Random number: {number}')
