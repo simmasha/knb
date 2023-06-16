5bf042af7ee4d0715f9a70621c45105999b6fcf6
simmasha <120249387+simmasha@users.noreply.github.com>
2023-06-16 08:52:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
 
+
 def generate_random_number():
     """
     Generates a random number between 1 and 100
     """
     return random.randint(1, 100)
+
 
 if __name__ == '__main__':
     number = generate_random_number()
